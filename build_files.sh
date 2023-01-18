e7ec2a25421f93c527c26cddd40191a1c6ae097a
iftesamulohy <64037194+iftesamulohy@users.noreply.github.com>
2023-01-18 14:11:09 +0600

    first commit it

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo " BUILD START"
 python3.9  -m pip install -r requirements.txt
+python3.9  -m pip install pysqlite3
+python3.9  -m pip install pysqlite3-binary
 python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
